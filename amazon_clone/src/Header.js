5f5acb1e1ee6fcb16342f4f7fb4b768ddc775e05
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2020-10-18 12:04:15 +0530

    added header and material ui

---
+++ amazon_clone/src/Header.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import './Header.css'
+
+export default function Header() {
+    return (
+        <div className='header'>
+            <img className="header_logo"
+            src='http://pngimg.com/uploads/amazon_PNG11.png'
+            />
+            <div className="header_search">
+<input className="header_searchInput" type="text"/>
+            </div>
+
+            <div className="header_nav">
+<div className="header_option">
+<span className="header_optionLineOne">
+Hello Guest
+</span>
+<span className="header_optionLineTwo">
+Sign In
+</span>
+</div>
+<div className="header_option">
+<span className="header_optionLineOne">
+Returns
+</span>
+<span className="header_optionLineTwo">
+& Orders
+</span>
+</div>
+<div className="header_option">
+<span className="header_optionLineOne">
+Your
+</span>
+<span className="header_optionLineTwo">
+Prime
+</span>
+</div>
+            </div>
+        </div>
+    )
+}
