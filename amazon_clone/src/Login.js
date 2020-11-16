bda2250670d514dc9d9a1773ed46c3d2d012d30f
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2020-11-16 14:16:53 +0530

    changed login.js

--- amazon_clone/src/Login.js
+++ amazon_clone/src/Login.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Login.css'
 
 function Login() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     return (
         <div className='login'>
             <Link to='/'>
@@ -16,12 +19,12 @@ function Login() {
 
                 <form>
                     <h5>E-mail</h5>
-                    <input type='text'  />
+                    <input type='text' value={email} onChange={e => setEmail(e.target.value)} />
 
                     <h5>Password</h5>
-                    <input type='password' v />
+                    <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
 
-                    <button type='submit' className='login_signInButton'>Sign In</button>
+                    <button type='submit'className='login__signInButton'>Sign In</button>
                 </form>
 
                 <p>
