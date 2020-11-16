e036595040e0b3d1d7efac7f65e0ed619e6409ab
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2020-11-17 00:30:18 +0530

    added firebase auth

--- amazon_clone/src/App.js
+++ amazon_clone/src/App.js
@@ -6,8 +6,29 @@ import Home from './Home';
 import Checkout from './Checkout';
 import Login from './Login'
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { useEffect } from 'react';
+import { useStateValue } from './StateProvider';
+import { auth } from './firebase';
 
 function App() {
+  const [{}, dispatch] = useStateValue(); 
+  useEffect(()=>{
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
+
+      if (authUser) {
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  },[])
   return (
     <Router>
        <div className="App">
